0476fe4c2db02da0536eec496c58c645f416cc00
wowbios <wowbios@yandex.ru>
2020-07-18 00:23:07 +0300

    feat: dependencies

---
+++ src/GameOfLifeNet/IRender.cs
@@ -0,0 +1,7 @@
+﻿namespace GameOfLifeNet
+{
+    public interface IRender
+    {
+        void Render(GameState state);
+    }
+}
---
+++ src/GameOfLifeNet/Preset/RandomPreset.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GameOfLifeNet.Preset
+{
+    public class RandomPreset : IPreset
+    {
+        private readonly int _fulFillPercent;
+
+        public RandomPreset(int fulFillPercent)
+        {
+            if(fulFillPercent < 0 || fulFillPercent > 100)
+                throw new ArgumentException("Fulfill must be positive and less than 100", nameof(fulFillPercent));
+            
+            _fulFillPercent = fulFillPercent;
+        }
+        
+        public void InitializeField(bool[,] field)
+        {
+            int width = field.GetLength(0);
+            int height = field.GetLength(1);
+            
+            var random = new Random();
+            int totalCount = width * height * _fulFillPercent / 100;
+            for (var i = 0; i < totalCount; i++)
+            {
+                int x = random.Next(0, width);
+                int y = random.Next(0, height);
+                field[x, y] = true;
+            }
+        }
+    }
+}

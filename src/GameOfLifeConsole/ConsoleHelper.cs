f26d231db9948110168712ad9c4aa3a258f16caf
wowbios <wowbios@yandex.ru>
2020-07-17 23:46:42 +0300

    GameOfLife init

---
+++ src/GameOfLifeConsole/ConsoleHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GameOfLifeConsole
+{
+    public static class ConsoleHelper
+    {
+        [DllImport("kernel32")]
+        private extern static bool SetConsoleFont(IntPtr hOutput, uint index);
+
+        private enum StdHandle
+        {
+            OutputHandle = -11
+        }
+
+        [DllImport("kernel32")]
+        private static extern IntPtr GetStdHandle(StdHandle index);
+
+        public static bool SetConsoleFont(uint index)
+        {
+            return SetConsoleFont(GetStdHandle(StdHandle.OutputHandle), index);
+        }
+
+        [DllImport("kernel32")]
+        private static extern bool GetConsoleFontInfo(IntPtr hOutput, [MarshalAs(UnmanagedType.Bool)]bool bMaximize,
+            uint count, [MarshalAs(UnmanagedType.LPArray), Out] ConsoleFont[] fonts);
+
+        [DllImport("kernel32")]
+        private static extern uint GetNumberOfConsoleFonts();
+
+        public static uint ConsoleFontsCount
+        {
+            get
+            {
+                return GetNumberOfConsoleFonts();
+            }
+        }
+
+        public static ConsoleFont[] ConsoleFonts
+        {
+            get
+            {
+                ConsoleFont[] fonts = new ConsoleFont[GetNumberOfConsoleFonts()];
+                if (fonts.Length > 0)
+                    GetConsoleFontInfo(GetStdHandle(StdHandle.OutputHandle), false, (uint)fonts.Length, fonts);
+                return fonts;
+            }
+        }
+
+    }
+}

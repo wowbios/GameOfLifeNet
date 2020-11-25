017f9cb8f1ec2bc491f4652e3380bab349044fb5
En3Tho <37334640+En3Tho@users.noreply.github.com>
2020-11-25 17:02:33 +0300

    Refactored multithread execution. Switched async.return to async CE in field processing

--- src/GameOfLife.FSharp/Game.fs
+++ src/GameOfLife.FSharp/Game.fs
@@ -1,5 +1,6 @@
 namespace GameOfLife.FSharp.Game
 
+open System.Collections.Concurrent
 open GameOfLife.Abstractions
 
 type ChangeEvent(x, y, isAlive) =
@@ -20,7 +21,7 @@ type GameSettings(width, height, preset, render, ruleset) =
         member this.Preset = preset
         member this.Render = render
         member this.Ruleset = ruleset
-        
+
 type Game(settings: IGameSettings) =
     let mutable field = Array2D.init settings.Width settings.Height (fun _ _ -> false)
     
@@ -40,36 +41,43 @@ type Game(settings: IGameSettings) =
                     ChangeEvent(i, j, field.[i, j]) |]
         
     let makeNextGen() =
-        let events = ResizeArray()
+        // single thread
+//        let events = ResizeArray<IChangeEvent>()
+//        let newField =
+//            field
+//            |> Array2D.mapi (fun x y value ->
+//                let isAlive = settings.Ruleset.IsAlive(field, x , y)
+//                match value = isAlive with
+//                | true -> value
+//                | false -> ChangeEvent(x,y, isAlive) :> IChangeEvent |> events.Add
+//                           isAlive)
+
+        // multi thread
+        let events = ConcurrentBag<IChangeEvent>()
+
         let newField = Array2D.copy field
         let width = Array2D.length1 field
         let height = Array2D.length2 field
-        
-        let processRow rowIndex rowWidth field  =        
-            seq {
-                for columnIndex in 0..rowWidth - 1 do
-                    let current = Array2D.get field rowIndex columnIndex
-                    let isA = settings.Ruleset.IsAlive(field, rowIndex, columnIndex)
-                    if current <> isA then
-                        newField.[rowIndex, columnIndex] <- isA
-                        ChangeEvent(rowIndex, columnIndex, isA)                    
-            }
-        
-        seq {
-            for rowIndex in 0..height - 1 do
-                processRow rowIndex width field |> async.Return
+
+        let processRow rowIndex rowWidth field =
+            for columnIndex in 0..rowWidth - 1 do
+                let current = Array2D.get field rowIndex columnIndex
+                let isAlive = settings.Ruleset.IsAlive(field, rowIndex, columnIndex)
+                if current <> isAlive then
+                    newField.[rowIndex, columnIndex] <- isAlive
+                    ChangeEvent(rowIndex, columnIndex, isAlive) |> events.Add
+
+        seq { for rowIndex in 0..height - 1 do
+                async { processRow rowIndex width field }
         }
         |> Async.Parallel
         |> Async.RunSynchronously
-        |> Array.iter (Seq.iter events.Add)
-        
+        |> ignore
+
         field <- newField
         generation <- generation + 1L
         
-        if events.Count > 0 then
-            render (events
-                    |> Seq.map (fun e -> e :> IChangeEvent)
-                    |> Seq.toArray)
+        events.ToArray() |> render
     
     interface IGame with
         member this.Prepare() = prepare()
